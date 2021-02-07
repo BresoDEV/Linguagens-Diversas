4a3a302b1e38cd610f0d3e02ec6b2dae6d994323
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2021-02-07 02:04:16 -0300

    Add files via upload

---
+++ C#/Sistema de Login C#/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.formAssistant1 = new DevExpress.XtraBars.FormAssistant();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 83);
+            this.simpleButton1.LookAndFeel.SkinName = "Xmas 2008 Blue";
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(223, 37);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Logar";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "DevExpress Dark Style";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.EditValue = "Usuario";
+            this.textEdit1.Location = new System.Drawing.Point(12, 31);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(223, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.EditValue = "Senha";
+            this.textEdit2.Location = new System.Drawing.Point(12, 57);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Properties.PasswordChar = '*';
+            this.textEdit2.Size = new System.Drawing.Size(223, 20);
+            this.textEdit2.TabIndex = 2;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(100, 137);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(46, 13);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "EdUaRdO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 162);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton1);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.None;
+            this.LookAndFeel.SkinName = "Valentine";
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraBars.FormAssistant formAssistant1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
---
+++ C#/Sistema de Login C#/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using DevExpress.UserSkins;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+
+        XmlDocument Xml = new XmlDocument();
+        public Form1()
+        {
+            
+          
+            InitializeComponent();
+            
+        }
+
+        public class InternetConnection
+        { 
+            private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+            public static bool IsConnectedToInternet()
+            {
+                bool returnValue = false;
+                try
+                {
+                    int Desc;
+                    returnValue = InternetGetConnectedState(out Desc, 0);
+                }
+                catch
+                {
+                    returnValue = false;
+                }
+                return returnValue;
+            }
+        }
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+
+        }
+
+        
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+                XmlDocument doc1 = new XmlDocument();
+                doc1.Load("http://www.bresosprx.com/oi.xml");
+                XmlElement root = doc1.DocumentElement;
+                XmlNodeList nodes = root.SelectNodes(textEdit1.Text);
+
+                foreach (XmlNode node in nodes)
+                {
+                    string usuario = node["Name"].InnerText;
+                    string senha = node["Senha"].InnerText;
+
+                    if (textEdit2.Text == senha)
+                    {
+                        MessageBox.Show("Bem vindo " + textEdit1.Text + ", voce foi autenticado com sucesso");
+                        Form2 novaform = new Form2();
+                        novaform.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show(textEdit1.Text + ", sua senha esta incorreta");
+                    }
+                }
+            
+           
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void simpleButton2_Click_1(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://www.bresosprx.com/oi.xml");
+            foreach (XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[0])
+                  
+                       MessageBox.Show(xmlNode.Attributes["Name"].Value);
+                   
+
+            
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool internetConnected = InternetConnection.IsConnectedToInternet();
+                MessageBox.Show(String.Format("Internet connection.{0}{1}",
+                   Environment.NewLine, internetConnected), "System Notification", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception ex)
+            {
+                // Add log if needed
+            }
+        }
+    }
+}
